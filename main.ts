efc90581ad38430ba0707297a8643c060d3e20f7
mhsob <94296728+mhsob@users.noreply.github.com>
2021-11-15 23:07:23 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let _4Digit: grove.TM1637 = null
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
 function rechts_drehen () {
     servos.P0.run(60)
     servos.P3.run(50)
@@ -6,7 +10,11 @@ function vorwaerts () {
     servos.P0.run(60)
     servos.P3.run(50)
 }
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
 basic.forever(function () {
+    _4Digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
     if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
         rechts_drehen()
     } else {
